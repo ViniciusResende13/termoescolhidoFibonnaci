db0f5462e0d5b3ba3fa65ea97922d8cbed38ed2e
Vinicius Resende do Carmo <105870043+ViniciusResende13@users.noreply.github.com>
2022-09-18 16:02:16 -0300

    Add files via upload

---
+++ ProgramFibonacciRecursão.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_com_Recursividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Digite o termo de sua escolha para a sequência de fibonacci: ");
+            n=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+            Console.WriteLine(fibo(n));
+
+            Console.ReadLine();
+        }
+        static int fibo(int n)
+        {
+            int res;
+
+            if(n == 1 || n == 2)
+            {
+                res= 1;
+            }
+            else
+            {
+                res= fibo(n-1) + fibo(n-2);
+            }
+            return res;
+        }
+    }
+}
